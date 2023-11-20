d0ac828183a51c142a6935ca00dc8de04f4ea655
yayanyang <yayanyang@gmail.com>
2023-11-20 17:14:06 +0800

    update quiche test codes

--- hala-quic/tests/quiche_test.rs
+++ hala-quic/tests/quiche_test.rs
@@ -1,4 +1,8 @@
-use quiche::ConnectionId;
+#![allow(unused)]
+
+use std::path::Path;
+
+use quiche::{ConnectionId, Header, RecvInfo, Type, MAX_CONN_ID_LEN};
 
 const MAX_DATAGRAM_SIZE: usize = 1350;
 
@@ -9,6 +13,28 @@ fn test_stream() {
     let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION).unwrap();
     config.set_application_protos(&[b"example-proto"]).unwrap();
     config.verify_peer(false);
+
+    let root_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+
+    log::debug!("test run dir {:?}", root_path);
+
+    config
+        .load_cert_chain_from_pem_file(root_path.join("tests/cert.crt").to_str().unwrap())
+        .unwrap();
+
+    config
+        .load_priv_key_from_pem_file(root_path.join("tests/cert.key").to_str().unwrap())
+        .unwrap();
+
+    config.set_max_idle_timeout(5000);
+    config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_initial_max_data(10_000_000);
+    config.set_initial_max_stream_data_bidi_local(1_000_000);
+    config.set_initial_max_stream_data_bidi_remote(1_000_000);
+    config.set_initial_max_streams_bidi(100);
+    config.set_initial_max_streams_uni(100);
+    config.set_disable_active_migration(true);
 
     let scid = ConnectionId::from_ref(b"client");
 
@@ -22,25 +48,144 @@ fn test_stream() {
         "client connection state is disconnected"
     );
 
-    let mut server_conn = quiche::accept(&scid, None, peer, local, &mut config).unwrap();
+    let mut out = [0; MAX_DATAGRAM_SIZE];
 
-    assert!(
-        !server_conn.is_established(),
-        "server connection state is disconnected"
+    /// handshake.
+    let (write_size, send_info) = client_conn.send(&mut out).unwrap();
+
+    log::info!(
+        "handshake len {}, client connection state: {:?}",
+        write_size,
+        client_conn.stats()
     );
 
-    let mut out = [0; MAX_DATAGRAM_SIZE];
+    let header = Header::from_slice(&mut out[..write_size], MAX_CONN_ID_LEN).unwrap();
+
+    log::info!("client handshake header: {:?}", header);
+
+    assert_eq!(header.ty, Type::Initial);
+
+    quiche::version_is_supported(header.version);
+
+    assert!(header.token.unwrap().is_empty());
+
+    let odcid = header.dcid.clone();
+
+    let len = quiche::retry(
+        &header.scid,
+        &header.dcid,
+        &ConnectionId::from_ref(b"server"),
+        b"server token",
+        header.version,
+        &mut out,
+    )
+    .unwrap();
+
+    let recv_info = RecvInfo {
+        from: send_info.to,
+        to: send_info.from,
+    };
+
+    /// client handle token
+    let read_size = client_conn.recv(&mut out[..len], recv_info).unwrap();
+
+    assert_eq!(read_size, len);
 
     let (write_size, send_info) = client_conn.send(&mut out).unwrap();
 
-    log::info!("client conn {:?} size {}", send_info, write_size);
+    log::info!(
+        "handshake len {}, client connection state: {:?}",
+        write_size,
+        client_conn.stats()
+    );
 
-    let recv_info = quiche::RecvInfo {
-        to: send_info.to,
+    let header = Header::from_slice(&mut out[..write_size], MAX_CONN_ID_LEN).unwrap();
+
+    log::info!("client header with token: {:?}", header);
+
+    assert_eq!(header.token.unwrap(), b"server token");
+
+    let mut server_conn = quiche::accept(
+        &ConnectionId::from_ref(b"server"),
+        Some(&odcid),
+        peer,
+        local,
+        &mut config,
+    )
+    .unwrap();
+
+    loop {
+        let recv_info = RecvInfo {
+            from: send_info.from,
+            to: send_info.to,
+        };
+
+        let read_size = server_conn.recv(&mut out[..write_size], recv_info).unwrap();
+
+        log::info!(
+            "{} processed {} bytes, state: {:?}",
+            server_conn.trace_id(),
+            read_size,
+            server_conn.stats()
+        );
+
+        let (write_size, _) = server_conn.send(&mut out).unwrap();
+
+        log::info!(
+            "{} send {} bytes, state: {:?}",
+            server_conn.trace_id(),
+            write_size,
+            server_conn.stats()
+        );
+
+        let recv_info = RecvInfo {
+            from: send_info.to,
+            to: send_info.from,
+        };
+
+        /// accept return
+        let read_size = client_conn.recv(&mut out[..write_size], recv_info).unwrap();
+
+        assert_eq!(read_size, write_size);
+
+        let (write_size, send_info) = client_conn.send(&mut out).unwrap();
+
+        log::info!(
+            "accept echo len {}, client connection state: {:?},{}",
+            write_size,
+            client_conn.stats(),
+            client_conn.is_established()
+        );
+
+        if client_conn.is_established() && server_conn.is_established() {
+            break;
+        }
+    }
+
+    assert!(client_conn.is_established());
+
+    let stream_write_size = client_conn
+        .stream_send(0x120, b"hello world", true)
+        .unwrap();
+
+    let (write_size, _) = client_conn.send(&mut out).unwrap();
+
+    let recv_info = RecvInfo {
         from: send_info.from,
+        to: send_info.to,
     };
 
     let read_size = server_conn.recv(&mut out[..write_size], recv_info).unwrap();
 
     assert_eq!(read_size, write_size);
+
+    assert!(server_conn.stream_readable(0x120));
+
+    let (stream_read_size, fin) = server_conn.stream_recv(0x120, &mut out).unwrap();
+
+    assert_eq!(stream_read_size, stream_write_size);
+
+    assert!(fin);
+
+    assert_eq!(&out[..stream_read_size], b"hello world");
 }
