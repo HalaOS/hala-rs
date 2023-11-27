e1ccd61c229fe2b44e392c6f60be1d40e6187aee
yayanyang <yayanyang@gmail.com>
2023-11-27 23:07:31 +0800

    update bench

--- hala-net/benches/udp_group.rs
+++ hala-net/benches/udp_group.rs
@@ -3,6 +3,7 @@ use std::net::SocketAddr;
 use divan::Bencher;
 use futures::executor::block_on;
 use hala_net::udp::{UdpGroup, UdpSocket};
+use hala_reactor::{ContextIoDevice, MTRunner};
 use rand::seq::SliceRandom;
 
 fn main() {
@@ -13,7 +14,7 @@ fn main() {
 #[divan::bench]
 fn udp_group(bencher: Bencher) {
     let (client_udp, group_udp, ports) = block_on(async {
-        let range = 10000..10010;
+        let range = 10000..11000;
 
         let client_udp: UdpSocket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
 
@@ -25,6 +26,8 @@ fn udp_group(bencher: Bencher) {
 
         (client_udp, group_udp, ports)
     });
+
+    hala_reactor::mt::MioDevice::get().run_loop(None);
 
     let send_buf = b"hello world";
 
@@ -45,6 +48,45 @@ fn udp_group(bencher: Bencher) {
             let mut buf = [0 as u8; 1024];
 
             let (_, recv_size, remote_addr) = group_udp.recv_from(&mut buf).await.unwrap();
+
+            log::trace!("recv from");
+
+            assert_eq!(recv_size, send_buf.len());
+
+            assert_eq!(remote_addr, client_udp.local_addr().unwrap());
+        };
+
+        block_on(bench_fut)
+    })
+}
+
+// 定义一个' fibonacci '函数并将其注册为基准测试
+#[divan::bench]
+fn udp(bencher: Bencher) {
+    let (server_udp, client_udp) = block_on(async {
+        let server_udp: UdpSocket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+
+        let client_udp: UdpSocket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+
+        (server_udp, client_udp)
+    });
+
+    hala_reactor::mt::MioDevice::get().run_loop(None);
+
+    let send_buf = b"hello world";
+
+    bencher.bench(move || {
+        let bench_fut = async {
+            client_udp
+                .send_to(send_buf, server_udp.local_addr().unwrap())
+                .await
+                .unwrap();
+
+            log::trace!("send to");
+
+            let mut buf = [0 as u8; 1024];
+
+            let (recv_size, remote_addr) = server_udp.recv_from(&mut buf).await.unwrap();
 
             log::trace!("recv from");
 
--- hala-net/src/lib.rs
+++ hala-net/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod tcp;
 pub mod udp;
+pub use hala_reactor as reactor;
