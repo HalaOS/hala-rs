9d47bd3a0e90709d604020b4dacc1b9a1c2617b8
yayanyang <yayanyang@gmail.com>
2023-12-01 19:27:18 +0800

    update

--- hala-io-driver/src/lib.rs
+++ hala-io-driver/src/lib.rs
@@ -1,17 +1,5 @@
-mod driver;
-mod fs;
-mod net;
-mod poller;
-mod time;
-
-pub use driver::*;
-pub use fs::*;
-pub use net::*;
-pub use poller::*;
-pub use time::*;
+#[cfg(feature = "mio_driver")]
+mod mio;
 
 #[cfg(feature = "mio_driver")]
-pub mod mio;
-
-#[cfg(feature = "notifier")]
-pub mod notifier;
+pub use mio::*;
