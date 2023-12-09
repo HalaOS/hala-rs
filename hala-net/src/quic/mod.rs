f59f149868292e9002187139328723a5908672a5
yayanyang <yayanyang@gmail.com>
2023-12-09 19:37:10 +0800

    update

--- hala-net/src/quic/mod.rs
+++ hala-net/src/quic/mod.rs
@@ -1,17 +1,5 @@
 mod config;
 pub use config::*;
 
-mod conn;
-pub use conn::*;
-
-mod stream;
-pub use stream::*;
-
-mod events;
-pub(crate) use events::*;
-
-mod event_loop;
-pub use event_loop::*;
-
 #[allow(unused)]
 pub(crate) const MAX_DATAGRAM_SIZE: usize = 1350;
