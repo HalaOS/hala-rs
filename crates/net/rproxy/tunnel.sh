0d1c19b4b1edfebcfe52b3a8342ab688fffa8cfe
yayanyang <yayanyang@gmail.com>
2024-01-26 12:11:28 +0800

    update

--- crates/net/rproxy/tunnel.sh
+++ crates/net/rproxy/tunnel.sh
@@ -1,5 +1,5 @@
 RUST_LOG=info
 
-nohup cargo run --example rproxy -- --laddrs 127.0.0.1:1812 -g tcp -t quic --peer-domain 127.0.0.1 --peer-port-range 1813 --tunnel-cert-chain-file ./cert/client.crt --tunnel-key-file ./cert/client.key > client.log &
+nohup cargo run --example rproxy --release -- --laddrs 127.0.0.1:1812 -g tcp -t quic --peer-domain 127.0.0.1 --peer-port-range 1813 --tunnel-cert-chain-file ./cert/client.crt --tunnel-key-file ./cert/client.key > client.log &
 
-nohup cargo run --example rproxy -- --laddrs 127.0.0.1:1813 -g quic -t tcp --peer-domain 127.0.0.1 --peer-port-range 12948 --gateway-cert-chain-file ./cert/server.crt --gateway-key-file ./cert/server.key > server.log &
+nohup cargo run --example rproxy --release -- --laddrs 127.0.0.1:1813 -g quic -t tcp --peer-domain 127.0.0.1 --peer-port-range 12948 --gateway-cert-chain-file ./cert/server.crt --gateway-key-file ./cert/server.key > server.log &
