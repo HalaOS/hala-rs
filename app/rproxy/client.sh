854c4485b1276d3eb468457741415f991e886083
yayanyang <yayanyang@gmail.com>
2024-02-04 17:01:03 +0800

    move rgnix to app/rproxy

---
+++ app/rproxy/client.sh
@@ -0,0 +1,3 @@
+RUST_LOG=info
+
+nohup cargo run --release --bin rproxy -- --laddrs 0.0.0.0:1812 -g tcp -t quic --raddrs 127.0.0.1:2000-2010 --tunnel-ca-file ./cert/hala_ca.pem --verify-server --tunnel-cert-chain-file ./cert/client.crt --tunnel-key-file ./cert/client.key > client.log &
--- rgnix/client.sh
+++
@@ -1,3 +0,0 @@
-RUST_LOG=info
-
-nohup cargo run --release --bin rgnix -- --laddrs 0.0.0.0:1812 -g tcp -t quic --raddrs 127.0.0.1:2000-2010 --tunnel-ca-file ./cert/hala_ca.pem --verify-server --tunnel-cert-chain-file ./cert/client.crt --tunnel-key-file ./cert/client.key > client.log &
