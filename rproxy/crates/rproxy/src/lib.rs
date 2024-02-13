b4777379296d4b7eb93d9c0134bec213a1fea89b
yayanyang <yayanyang@gmail.com>
2024-02-13 18:51:42 +0800

    update

--- rproxy/crates/rproxy/src/lib.rs
+++ rproxy/crates/rproxy/src/lib.rs
@@ -1,36 +1,85 @@
-use std::{any::Any, io};
+use std::{any::Any, fmt::Display, io};
 
-use futures::channel::mpsc;
-use hala_rs::io::bytes::BytesMut;
+use dashmap::DashMap;
+use futures::{AsyncRead, AsyncWrite};
 use uuid::Uuid;
 
-pub trait Service: Send + Sync {
-    /// Get the ID of the component to which the service belongs
-    fn compoent_id(&self) -> Uuid;
-}
+/// The gateway protocol should implement this trait.
+pub trait Gateway {
+    /// Gateway protocol creation config type.
+    type Config;
 
-pub trait Component {
-    fn compoent_id() -> Uuid;
-}
+    /// Incoming stream handler type.
+    type Handler: Handler;
 
-pub trait Gateway: Component {
-    /// Create and startup new gateway service with provided open config.
-    fn startup(&self, config: &dyn Any) -> io::Result<Box<dyn Service>>;
+    /// Create a new instance of the `Gateway` protocol using the provided `config` and [`Handler`]
+    fn new(config: Self::Config, handler: Self::Handler) -> io::Result<Self>
+    where
+        Self: Sized;
 }
 
 /// Stream abstract for [`RustProxy`]
-pub struct Stream {
+pub struct Stream<E, R, W> {
     /// Global tracking ID.
     pub id: Uuid,
-    /// The write half of the stream.
-    pub write: mpsc::Sender<BytesMut>,
+    /// Stream from endpoint.
+    pub from: E,
+    /// Stream to endpoint.
+    pub to: E,
     /// The read half of the stream.
-    pub read: mpsc::Receiver<BytesMut>,
+    pub read: R,
+    /// The write half of the stream.
+    pub write: W,
 }
 
+/// [`Stream`] handler for incoming [`Stream`]
 pub trait Handler {
-    /// Handle next incoming stream. if this handler can't handle the stream.5
-    fn next(&self, stream: Stream) -> io::Result<()>;
+    /// Handle next incoming stream.
+    fn next<E, R, W>(&self, stream: Stream<E, R, W>) -> io::Result<()>
+    where
+        R: AsyncRead,
+        W: AsyncWrite;
 }
 
-pub struct RustProxy {}
+/// Main enter for rproxy.
+pub struct RustProxy<H> {
+    /// mixin stream [`Handler`]
+    pub handler: H,
+    /// startup gateway services.
+    pub gateway_services: DashMap<Uuid, Box<dyn Any>>,
+}
+
+impl<H> RustProxy<H>
+where
+    H: Handler + Clone,
+{
+    /// Create new [`RustProxy`] instance using provided [`Handler`]
+    pub fn new(handler: H) -> Self {
+        Self {
+            handler,
+            gateway_services: Default::default(),
+        }
+    }
+
+    /// Startup new gateway instance.
+    pub fn startup<G: Gateway<Handler = H> + Any>(&self, config: G::Config) -> io::Result<Uuid> {
+        let service = Box::new(G::new(config, self.handler.clone())?);
+
+        let id = Uuid::new_v4();
+
+        self.gateway_services.insert(id, service);
+
+        Ok(id)
+    }
+
+    /// Stop gateway instance by id.
+    pub fn stop(&self, id: &Uuid) -> io::Result<()> {
+        self.gateway_services
+            .remove(id)
+            .map(|_| ())
+            .ok_or(io::Error::new(
+                io::ErrorKind::NotFound,
+                format!("gateway service not found, id={}", id),
+            ))
+    }
+}
