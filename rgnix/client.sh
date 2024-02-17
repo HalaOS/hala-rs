99a09473a3467a1e66a257f72166e57ade531a8c
yayanyang <yayanyang@gmail.com>
2024-02-17 22:42:56 +0800

    update

--- rgnix/client.sh
+++ rgnix/client.sh
@@ -1,3 +1,3 @@
 RUST_LOG=info
 
-nohup cargo run --release --features="client" --bin qtun-client -- --laddrs 0.0.0.0:1812 --raddrs 127.0.0.1:2000-2010 --ca-file ./cert/hala_ca.pem --cert-chain-file ./cert/client.crt --key-file ./cert/client.key > client.log &
+nohup cargo run --release --features="client" --bin qtun-client -- --laddrs 0.0.0.0:1812 --raddrs 127.0.0.1:2000-3000 --ca-file ./cert/hala_ca.pem --cert-chain-file ./cert/client.crt --key-file ./cert/client.key > client.log &
