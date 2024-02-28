faabb3fe12d6d38dbf8fffcb5c47b51791a789fa
yayanyang <yayanyang@gmail.com>
2024-02-28 12:37:04 +0800

    update

--- crates/net/http/src/parse.rs
+++ crates/net/http/src/parse.rs
@@ -1,9 +1,12 @@
 use std::io;
 
-use bytes::Bytes;
 use futures::{AsyncRead, AsyncReadExt, AsyncWrite};
 use hala_io::ReadBuf;
-use http::{uri::InvalidUri, HeaderMap, HeaderValue, Method, Request, Uri, Version};
+use http::{
+    header::{InvalidHeaderName, InvalidHeaderValue},
+    uri::InvalidUri,
+    HeaderMap, HeaderName, HeaderValue, Method, Request, Uri, Version,
+};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -25,6 +28,33 @@ pub enum ParseError {
 
     #[error("Unsupport http uri: {0}")]
     InvalidUri(#[from] InvalidUri),
+
+    #[error("Unsupport http version.")]
+    InvalidVersion,
+
+    #[error("New line token error, expect \r\n")]
+    Newline,
+
+    #[error("Header name is empty.")]
+    HeaderName,
+
+    #[error("{0}")]
+    InvalidHeaderName(#[from] InvalidHeaderName),
+
+    #[error("Header value is empty.")]
+    HeaderValue,
+
+    #[error("{0}")]
+    InvalidHeaderValue(#[from] InvalidHeaderValue),
+}
+
+impl From<ParseError> for io::Error {
+    fn from(value: ParseError) -> Self {
+        match value {
+            ParseError::Io(io) => io,
+            _ => io::Error::new(io::ErrorKind::Other, value),
+        }
+    }
 }
 
 /// Type alias of [`Result<T, ParseError>`]
@@ -33,14 +63,12 @@ pub type ParseResult<T> = Result<T, ParseError>;
 /// The http [`Request`](http::Request), [`Response`](http::Response) parser config.
 pub struct ParserConfig {
     header_parse_max_buf: usize,
-    allow_multiple_spaces_in_request_line_delimiters: bool,
 }
 
 impl Default for ParserConfig {
     fn default() -> Self {
         Self {
             header_parse_max_buf: 2048,
-            allow_multiple_spaces_in_request_line_delimiters: false,
         }
     }
 }
@@ -63,12 +91,41 @@ struct RequestParsing {
     headers: HeaderMap<HeaderValue>,
 }
 
+/// Inner func [`RequestParser::skip_newline`] result variant.
+enum SkipNewLine {
+    /// do nothing
+    None,
+    /// skip one newline token.
+    One,
+    /// This is a sequence of two line breaks, indicating that processing
+    /// of the current paragraph has been completed.
+    Break,
+    /// newline token is incomplete.
+    Incomplete,
+}
+
+/// Inner func [`RequestParser::skip_newline`] result variant.
+enum ParseHeader {
+    /// parse one header name / value pair.
+    One,
+    /// the parsed is the last one item of header field.
+    Last,
+    /// incomplete header item
+    Incomplete,
+}
+
 /// The parser which parse incoming `stream` into [`Request`](http::Request).
 pub struct RequestParser<S> {
+    /// tag for log information output.
     debug_info: String,
+    /// http protocol parse config.
+    #[allow(unused)]
     parser_config: ParserConfig,
+    /// head parse buf.
     read_buf: ReadBuf,
+    /// read http request packet from this stream.
     stream: S,
+    /// the parser statemachine.
     parsing: RequestParsing,
 }
 
@@ -98,7 +155,7 @@ where
 {
     ///  Parse and generate [`Request`] object.
     pub async fn parse(mut self) -> ParseResult<Request<S>> {
-        loop {
+        'outer: loop {
             let read_size = self.stream.read(self.read_buf.chunk_mut()).await?;
 
             // reach stream EOF.
@@ -108,35 +165,123 @@ where
 
             self.read_buf.filled(read_size);
 
-            while self.read_buf.chunk().len() > 0 {
+            'inner: while self.read_buf.chunk().len() > 0 {
                 match self.parsing.state {
                     // parse method.
                     0 => {
                         self.parse_method()?;
+                        self.skip_spaces();
                     }
                     // parse uri
                     1 => {
                         self.parse_uri()?;
+                        self.skip_spaces();
                     }
-                    2 => {}
-                    3 => {}
+                    // parse version: HTTP/x.x
+                    2 => {
+                        self.parse_version()?;
+                    }
+                    3 => match self.parse_headers()? {
+                        ParseHeader::Last => {
+                            break 'outer;
+                        }
+                        ParseHeader::Incomplete => {
+                            break 'inner;
+                        }
+                        _ => {}
+                    },
                     _ => {
                         panic!("Not here, inner error.");
                     }
                 };
             }
-
-            self.skip_spaces();
         }
+
+        todo!()
     }
 
     fn skip_spaces(&mut self) {
-        if self
-            .parser_config
-            .allow_multiple_spaces_in_request_line_delimiters
-        {
-            self.read_buf.split_to(skip_spaces(self.read_buf.chunk()));
+        self.read_buf.split_to(skip_spaces(self.read_buf.chunk()));
+    }
+
+    fn skip_newline(&mut self) -> ParseResult<SkipNewLine> {
+        match skip_newline(self.read_buf.chunk())? {
+            SkipNewLine::One => {
+                self.read_buf.split_to(2);
+                Ok(SkipNewLine::One)
+            }
+            SkipNewLine::Break => {
+                self.read_buf.split_to(4);
+
+                Ok(SkipNewLine::Break)
+            }
+            status => Ok(status),
         }
+    }
+
+    fn parse_headers(&mut self) -> ParseResult<ParseHeader> {
+        match self.skip_newline()? {
+            SkipNewLine::Break => return Ok(ParseHeader::Last),
+            SkipNewLine::Incomplete => return Ok(ParseHeader::Incomplete),
+            _ => {}
+        }
+
+        // parse name.
+        let buf = self.read_buf.chunk();
+
+        let mut header_name = None;
+
+        for (offset, c) in buf.iter().cloned().enumerate() {
+            if c == b':' {
+                if offset == 0 {
+                    return Err(ParseError::HeaderName);
+                }
+
+                let buf = self.read_buf.split_to(offset + 1);
+
+                header_name = Some(HeaderName::from_bytes(&buf[0..(buf.len() - 1)])?);
+
+                break;
+            }
+        }
+
+        // Incomplete header name.
+        if header_name.is_none() {
+            return Ok(ParseHeader::Incomplete);
+        }
+
+        // skip space between ':' and value.
+        self.skip_spaces();
+
+        // parse value.
+        let buf = self.read_buf.chunk();
+
+        let mut header_value = None;
+
+        for (offset, c) in buf.iter().cloned().enumerate() {
+            if c == b'\r' {
+                if offset == 0 {
+                    return Err(ParseError::HeaderValue);
+                }
+
+                let buf = self.read_buf.split_to(offset);
+
+                header_value = Some(HeaderValue::from_maybe_shared(buf)?);
+
+                break;
+            }
+        }
+
+        // Incomplete header value.
+        if header_name.is_none() {
+            return Ok(ParseHeader::Incomplete);
+        }
+
+        self.parsing
+            .headers
+            .insert(header_name.unwrap(), header_value.unwrap());
+
+        Ok(ParseHeader::One)
     }
 
     fn parse_method(&mut self) -> ParseResult<()> {
@@ -188,6 +333,24 @@ where
             Err(err) => Err(err.into()),
         }
     }
+
+    fn parse_version(&mut self) -> ParseResult<()> {
+        let token = parse_version(self.read_buf.chunk())?;
+
+        if token.is_none() {
+            return Ok(());
+        }
+
+        let token = token.unwrap();
+
+        self.read_buf.split_to(8);
+
+        self.parsing.version = Some(token);
+
+        self.parsing.state += 1;
+
+        Ok(())
+    }
 }
 
 /// Determines if byte is a token char.
@@ -236,12 +399,44 @@ macro_rules! byte_map {
 #[inline]
 fn skip_spaces(buf: &[u8]) -> usize {
     for (offset, b) in buf.iter().cloned().enumerate() {
-        if b != b' ' {
+        if b != b' ' || b != b'\t' {
             return offset;
         }
     }
 
     buf.len()
+}
+
+#[inline]
+fn _skip_newline(buf: &[u8]) -> ParseResult<SkipNewLine> {
+    if buf.len() > 1 {
+        if b"\r\n" == &buf[..2] {
+            return Ok(SkipNewLine::One);
+        }
+    } else if buf.len() > 0 {
+        match buf[0] {
+            b'\n' => {
+                return Err(ParseError::Newline);
+            }
+            b'\r' => {
+                return Ok(SkipNewLine::Incomplete);
+            }
+            _ => {}
+        }
+    }
+
+    Ok(SkipNewLine::None)
+}
+
+#[inline]
+fn skip_newline(buf: &[u8]) -> ParseResult<SkipNewLine> {
+    match _skip_newline(buf)? {
+        SkipNewLine::One => match _skip_newline(buf)? {
+            SkipNewLine::One => Ok(SkipNewLine::Break),
+            status => Ok(status),
+        },
+        status => Ok(status),
+    }
 }
 
 // ASCII codes to accept URI string.
@@ -296,3 +491,22 @@ fn parse_uri(buf: &[u8]) -> ParseResult<Option<&[u8]>> {
     // The token data is not complete, return `None`
     Ok(None)
 }
+
+#[inline]
+fn parse_version(buf: &[u8]) -> ParseResult<Option<Version>> {
+    if buf.len() >= 8 {
+        return match &buf[0..8] {
+            b"HTTP/0.9" => Ok(Some(Version::HTTP_09)),
+            b"HTTP/1.0" => Ok(Some(Version::HTTP_10)),
+            b"HTTP/1.1" => Ok(Some(Version::HTTP_11)),
+            b"HTTP/2.0" => Ok(Some(Version::HTTP_2)),
+            b"HTTP/3.0" => Ok(Some(Version::HTTP_3)),
+            _ => Err(ParseError::InvalidVersion),
+        };
+    }
+
+    Ok(None)
+}
+
+#[cfg(test)]
+mod tests {}
